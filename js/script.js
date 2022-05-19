cd9c6002ed27507af9637a14c51aa7ffc9c26563
marinamedeirosz <marinapaffer@gmail.com>
2022-05-19 14:19:21 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,60 @@
+function aumentaBraco(){
+    let valor = document.getElementById("braco");
+    if (valor.value < 10 && valor.value >=0){
+        valor.value = parseInt(valor.value) + 1;
+    }
+}
+function diminuiBraco(){
+    let valor = document.getElementById("braco");
+    if (valor.value <= 10 && valor.value >0){
+        valor.value = parseInt(valor.value) - 1;
+    }
+}
+function aumentaBlindagem(){
+    let valor = document.getElementById("blindagem");
+    if (valor.value < 10 && valor.value >=0){
+        valor.value = parseInt(valor.value) + 1;
+    }
+}
+function diminuiBlindagem(){
+    let valor = document.getElementById("blindagem");
+    if (valor.value <= 10 && valor.value >0){
+        valor.value = parseInt(valor.value) - 1;
+    }
+}
+function aumentaNucleo(){
+    let valor = document.getElementById("nucleo");
+    if (valor.value < 10 && valor.value >=0){
+        valor.value = parseInt(valor.value) + 1;
+    }
+}
+function diminuiNucleo(){
+    let valor = document.getElementById("nucleo");
+    if (valor.value <= 10 && valor.value >0){
+        valor.value = parseInt(valor.value) - 1;
+    }
+}
+function aumentaPerna(){
+    let valor = document.getElementById("perna");
+    if (valor.value < 10 && valor.value >=0){
+        valor.value = parseInt(valor.value) + 1;
+    }
+}
+function diminuiPerna(){
+    let valor = document.getElementById("perna");
+    if (valor.value <= 10 && valor.value >0){
+        valor.value = parseInt(valor.value) - 1;
+    }
+}
+function aumentaFoguete(){
+    let valor = document.getElementById("foguete");
+    if (valor.value < 10 && valor.value >=0){
+        valor.value = parseInt(valor.value) + 1;
+    }
+}
+function diminuiFoguete(){
+    let valor = document.getElementById("foguete");
+    if (valor.value <= 10 && valor.value >0){
+        valor.value = parseInt(valor.value) - 1;
+    }
+}
